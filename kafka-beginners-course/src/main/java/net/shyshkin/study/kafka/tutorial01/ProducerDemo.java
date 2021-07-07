9cf680f06eeeebdc7bd79cf35b8780c11a3ab45f
Art <d.art.shishkin@gmail.com>
2021-07-07 13:44:21 +0300

    44. Creating Kafka Project (#3 Section 7: Kafka Java Programming 101)

---
+++ kafka-beginners-course/src/main/java/net/shyshkin/study/kafka/tutorial01/ProducerDemo.java
@@ -0,0 +1,13 @@
+package net.shyshkin.study.kafka.tutorial01;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ProducerDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        log.info("Demo is starting...");
+    }
+
+}
