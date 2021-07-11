1eec79d56a18e6676f1c99bf5f56657ea515606b
Art <d.art.shishkin@gmail.com>
2021-07-11 14:51:00 +0300

    56. Twitter Setup - Hosebird Client (hbc) dependency (#5)

---
+++ kafka-real-world-twitter/src/main/java/net/shyshkin/study/kafka/twitter/KafkaConfiguration.java
@@ -0,0 +1,37 @@
+package net.shyshkin.study.kafka.twitter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+@Slf4j
+public class KafkaConfiguration {
+
+    public static final String TOPIC = "java-test-topic";
+    public static final String SERVERS = "localhost:29092";
+
+    private static final String DEFAULT_CONSUMER_GROUP_ID = "my-fours-application";
+
+    public static Properties getProducerProperties() {
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, SERVERS);
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return properties;
+    }
+
+    public static Properties getConsumerProperties() {
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, SERVERS);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, DEFAULT_CONSUMER_GROUP_ID);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        return properties;
+    }
+
+}
